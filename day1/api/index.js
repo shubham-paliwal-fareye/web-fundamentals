72ac87ffdc0d81556581e456b21204984188aa89
divyraj1 <divyraj1@getfareye.com>
2022-10-13 12:27:50 +0530

    fc

--- day1/api/index.js
+++ day1/api/index.js
@@ -1,16 +1,28 @@
 // How to fetch Data from server.
 
 function fetchDataFromServer() {
-  fetch("https://jsonplaceholder.typicode.com/todos")
+  fetch("https://dummyjson.com/users")
     .then((response) => response.json())
     .then((json) => printData(json));
 }
 
 function printData(data) {
   console.log(data);
-  let ele = document.getElementById("code");
-  let tempData = data.filter((a) => a.id < 50);
-  ele.innerHTML = JSON.stringify(tempData, null, 4);
-}
+  //let ele = document.getElementById("code");
+  //let tempData = data.filter((a) => a.id < 50);
+  //console.log(tempData)
+ // ele.innerHTML = JSON.stringify(tempData, null, 4);
 
+//  fruits.forEach(myFunction);
+ let {limit,skip,total,users}=data
+//      console.log(users)
+// }
+let fullnames=users.map(user=>user.firstName+" "+user.lastName)
+
+let filtermap =users.filter(user=>user.age<50);
+console.log("filter Map"+filtermap)
+
+
+console.log(fullnames)
+}
 fetchDataFromServer();
